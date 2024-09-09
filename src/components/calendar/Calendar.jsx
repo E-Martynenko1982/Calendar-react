a07dcc39e93c8cab70deea2b833c1afe6048aa45
E-Martynenko1982 <motor8103@gmail.com>
2024-09-09 10:22:23 +0300

    update

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -3,25 +3,18 @@ import PropTypes from "prop-types";
 import Navigation from './../navigation/Navigation.jsx';
 import Week from '../week/Week';
 import Sidebar from '../sidebar/Sidebar.jsx';
-//import RedLine from '../red-line/RedLine.jsx';
+
 
 import './calendar.scss';
 
 const Calendar = ({ weekDates, events, deleteEvent, openModal }) => {
-  // const isToday = (dateArray) => {
-  //   const today = new Date();
-  //   return dateArray.some(date =>
-  //     date.getDate() === today.getDate() &&
-  //     date.getMonth() === today.getMonth() &&
-  //     date.getFullYear() === today.getFullYear()
-  //   );
-  // };
+
   return (
     <section className="calendar">
       <Navigation weekDates={weekDates} />
       <div className="calendar__body">
         <div className="calendar__week-container">
-          {/* {isToday(weekDates) && <RedLine />} */}
+
           <Sidebar />
           <Week
             weekDates={weekDates}
